b9dd33c7564a5ebe8e1eb47238d5300ba4eef804
LiamS57 <william.seymour656@topper.wku.edu>
2024-07-17 09:40:41 -0500

    log loading rework

--- analysis/log_loader.py
+++ analysis/log_loader.py
@@ -41,6 +41,12 @@ class TaggedDataList:
 
     def with_tags(self, *tags) -> list[TaggedData]:
         return [tagged for tagged in self._list if tagged.has_tags(list(tags))]
+    
+    def without_tags(self, *tags) -> list[TaggedData]:
+        return [tagged for tagged in self._list if not tagged.has_tags(list(tags))]
+    
+    def with_and_without_tags(self, tags_with: list, tags_without: list) -> list[TaggedData]:
+        return [tagged for tagged in self._list if tagged.has_tags(tags_with) and not tagged.has_tags(tags_without)]
 
 def _get_timestamp_period(log: Log, flag: str) -> tuple[float, float]:
     start = log.get_timestamp(f'{flag}_START')
@@ -57,7 +63,6 @@ def load_logs_from_folder(in_folder: str) -> TaggedDataList:
 
     # load from files into a list of logs associated with name fragments
     loaded: list[tuple[list[str], Log]] = list()
-    num_loaded = 0
     for filename in os.listdir(in_folder):
         if filename.endswith('.json'):
             try:
@@ -71,95 +76,103 @@ def load_logs_from_folder(in_folder: str) -> TaggedDataList:
                 #name_data.insert(0, name_data.pop()) # move suffix to the beginning of the name data
 
                 loaded.append((name_data, log_data))
-                num_loaded += 1
             except:
                 pass
-    print(f'Loaded {num_loaded} files')
+    print(f'Loaded {len(loaded)} files')
 
-    # move loaded data into tagged data
-    tagged_data_list: list[TaggedData] = list()
+    # move loaded data into dict, grouping iterations together
+    loaded_iter_groups: dict[str, list[tuple[int, Log]]] = dict()
     for name, log in sorted(loaded, key=lambda x: x[0]):
         i = int(name.pop(1)) # this fragment is an iteration number, the rest are used as tags
+        name_str = '_'.join(name)
 
-        period_names = list()
-        for pname in [x.value[:-6] for x in log.timestamps if x.value.endswith('_START')]:
-            if log.get_timestamp(f'{pname}_END') != -1:
-                period_names.append(pname)
+        if name_str not in loaded_iter_groups:
+            loaded_iter_groups[name_str] = list()
         
-        period_list: list[LogPeriod] = list()
-        for pname in period_names:
-            # fill LogPeriod with data
-
-            log_period = LogPeriod()
-            log_period.name = pname
-            log_period.i = i # TODO: REMOVE
-
-            log_period.accuracy = log.accuracy
-            log_period.tokens_generated = log.tokens_generated
-
-            start, end = _get_timestamp_period(log, pname)
-            log_period.length = end - start
-
-            tmp: list[tuple[float, float]] = list()
-            for entry in log.freq_gpu:
-                if entry.time >= start and entry.time <= end:
-                    tmp.append((entry.time - start, entry.value))
-            log_period.freq_gpu = np.array(tmp)
-            
-            tmp: list[tuple[float, float]] = list()
-            # just going to add up total if there are multiple pids
-            for pid, entries in log.memory_ram.items():
-                for entry in entries:
-                    if entry.time >= start and entry.time <= end:
-                        p_t = entry.time - start
-                        flag = False
-                        for prev_ram in tmp:
-                            if not flag and prev_ram[0] == p_t:
-                                flag = True
-                                prev_ram[1] += entry.value
-                        if not flag:
-                            tmp.append((p_t, entry.value))
-            log_period.memory_ram = np.array(tmp)
-            
-            tmp: list[tuple[float, float]] = list()
-            # just going to add up total if there are multiple pids
-            for pid, entries in log.memory_gpu.items():
-                for entry in entries:
-                    if entry.time >= start and entry.time <= end:
-                        p_t = entry.time - start
-                        flag = False
-                        for prev_gpu in tmp:
-                            if not flag and prev_gpu[0] == p_t:
-                                flag = True
-                                prev_gpu[1] += entry.value
-                        if not flag:
-                            tmp.append((p_t, entry.value))
-            log_period.memory_gpu = np.array(tmp)
-
-            tmp: list[tuple[float, float]] = list()
-            for entry in log.power:
-                if entry.time >= start and entry.time <= end:
-                    tmp.append((entry.time - start, entry.value))
-            log_period.power = np.array(tmp)
-            
-            period_list.append(log_period)
+        loaded_iter_groups[name_str].append((i, log))
+        loaded_iter_groups[name_str] = sorted(loaded_iter_groups[name_str], key=lambda x: x[0])
+    
+    # move grouped data into tagged data
+    tagged_data_list: list[TaggedData] = list()
+    for name_str, iter_list in loaded_iter_groups.items():
+        tagged_data = TaggedData()
+        tagged_data._tags = name_str.split('_')
         
-        # save periods to the proper TaggedData
-        flag_used_old = False
-        for prev_tdata in tagged_data_list:
-            if not flag_used_old and prev_tdata.has_tags(name):
-                # use the old TaggedData
-                flag_used_old = True
-                prev_tdata._periods.append(period_list)
-        if not flag_used_old:
-            # make a new TaggedData
-            tdata = TaggedData()
-            tdata._tags = name
-            tdata._periods = list()
-            tdata._periods.append(period_list)
-            tagged_data_list.append(tdata)
+        iter_periods_list: list[list[LogPeriod]] = list()
+        for i, log in iter_list:
+            # get period names in iteration
+            period_names = list()
+            for pname in [x.value[:-6] for x in log.timestamps if x.value.endswith('_START')]:
+                if log.get_timestamp(f'{pname}_END') != -1:
+                    period_names.append(pname)
+            
+            period_list: list[LogPeriod] = list()
+            for pname in period_names:
+                # fill LogPeriod with data
+
+                log_period = LogPeriod()
+                log_period.name = pname
+                log_period.i = i # TODO: REMOVE
+
+                log_period.accuracy = log.accuracy
+                log_period.tokens_generated = log.tokens_generated
+
+                start, end = _get_timestamp_period(log, pname)
+                log_period.length = end - start
+
+                tmp: list[tuple[float, float]] = list()
+                for entry in log.freq_gpu:
+                    if entry.time >= start and entry.time <= end:
+                        tmp.append((entry.time - start, entry.value))
+                log_period.freq_gpu = np.array(tmp)
+                
+                tmp: list[tuple[float, float]] = list()
+                # just going to add up total if there are multiple pids
+                for pid, entries in log.memory_ram.items():
+                    for entry in entries:
+                        if entry.time >= start and entry.time <= end:
+                            p_t = entry.time - start
+                            flag = False
+                            for prev_ram in tmp:
+                                if not flag and prev_ram[0] == p_t:
+                                    flag = True
+                                    prev_ram[1] += entry.value
+                            if not flag:
+                                tmp.append((p_t, entry.value))
+                log_period.memory_ram = np.array(tmp)
+                
+                tmp: list[tuple[float, float]] = list()
+                # just going to add up total if there are multiple pids
+                for pid, entries in log.memory_gpu.items():
+                    for entry in entries:
+                        if entry.time >= start and entry.time <= end:
+                            p_t = entry.time - start
+                            flag = False
+                            for prev_gpu in tmp:
+                                if not flag and prev_gpu[0] == p_t:
+                                    flag = True
+                                    prev_gpu[1] += entry.value
+                            if not flag:
+                                tmp.append((p_t, entry.value))
+                log_period.memory_gpu = np.array(tmp)
+
+                tmp: list[tuple[float, float]] = list()
+                for entry in log.power:
+                    if entry.time >= start and entry.time <= end:
+                        tmp.append((entry.time - start, entry.value))
+                log_period.power = np.array(tmp)
+                
+                period_list.append(log_period)
+
+            iter_periods_list.append(period_list)
+        
+        # save periods and append tagged data
+        tagged_data._periods = sorted(iter_periods_list, key=lambda x: x[0].i)
+        tagged_data_list.append(tagged_data)
+    
 
     # move everything into an instance of TaggedDataList and return
+    print(f'Processed {len(tagged_data_list)} configurations')
     ret = TaggedDataList()
     ret._list = tagged_data_list
     return ret
